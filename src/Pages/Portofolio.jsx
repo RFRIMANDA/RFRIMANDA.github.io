8853d8dcdccc6fcc0ac3db8e868d894afb4da5f2
RFRIMANDA <rifkyfrimanda@gmail.com>
2025-08-09 21:59:52 +0700

    new sabtuu

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -205,7 +205,7 @@ export default function FullWidthTabs() {
             backgroundClip: 'text',
             WebkitTextFillColor: 'transparent'
           }}>
-            Portofolio Showcase
+            Portfolio Showcase
           </span>
         </h2>
         <p className="text-slate-400 max-w-2xl mx-auto text-sm md:text-base mt-2">
