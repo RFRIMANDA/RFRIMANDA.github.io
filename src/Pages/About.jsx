bd5288490d5d6b04e4c1a58c93e4f46c2ef613f0
RFRIMANDA <rifkyfrimanda@gmail.com>
2025-08-24 03:37:38 +0700

    neww

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -240,7 +240,7 @@ const AboutPage = () => {
       </div>
  
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1Mppy3kkVUeJ6vJnq68DOyJIf2gO6l0u6/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/125iq4BsXrlOGuS8XNKsORzKma7L9AKip/view?usp=sharing" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
